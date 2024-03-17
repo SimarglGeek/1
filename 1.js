a21e8e3d755e4c7b1bd6a0cd0a888efc95d6ff16
SimarglGeek <info@koverclub.com>
2024-03-17 23:36:39 +0300

    first commit

---
+++ 1.js
@@ -0,0 +1,12 @@
+let text = 'upcoming collection...';
+let content = document.querySelector('#content');
+
+for (let i in [...text]) {
+  let letter = document.createElement('span');
+  letter.textContent = [...text][i];
+  if (letter.textContent.match(/\s/)) {
+    letter.style.margin = 'auto 3px';
+  }
+  letter.style.animationDelay = i / 10 + 's';
+  content.appendChild(letter);
+}
